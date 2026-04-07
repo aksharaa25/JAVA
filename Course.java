80e90c1f4e5648d05422a24099d4f5e113de936e
Akshara A <aksharaa4549688@gmail.com>
2026-04-07 11:42:44 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,8 @@
+public class Course {
+    public static void main(String[] args) {
+        int rollno=20;
+        String course="MCA";
+        System.out.println("ROLLNO:"+rollno);
+        System.out.println("COURSE:"+course);
+    }
+}
